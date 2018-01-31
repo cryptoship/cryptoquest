18b0476667ca9aa0a266d2da8240ce217c7e95a3
Mike Jonas <motion@gmail.com>
2018-01-31 00:38:06 -0800

    Get routing working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { Switch, Route } from 'react-router-dom'
+import { withRouter } from 'react-router'
+
 import './App.css'
 
-import Pillow from './components/Pillow'
+import Index from './pages/Index'
+import About from './pages/About'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <Pillow />
-        </p>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/about" component={About} />
+        </Switch>
       </div>
     )
   }
 }
 
-export default App
+export default withRouter(App)
