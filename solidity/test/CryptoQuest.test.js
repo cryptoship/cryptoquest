8cf1b25b2dd13c9acfce85564996fcb2bd6218b5
Daniel Kurka <kurka.daniel@gmail.com>
2018-02-02 17:43:59 -0800

    First working test.

---
+++ solidity/test/CryptoQuest.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const {interface, bytecode} = require('../compile');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+let accounts;
+let cryptoQuest;
+
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts();
+	
+	cryptoQuest = await new web3.eth.Contract(JSON.parse(interface))
+	    .deploy({ data: bytecode})
+	    .send({from: accounts[0], gas : '5000000'})
+	
+	cryptoQuest.setProvider(provider);
+});
+
+describe('CryptoQuest', () => {
+	it('deploys a contract', () => {
+		console.log('Address: ', cryptoQuest.options.address);
+		assert.ok(cryptoQuest.options.address)
+	});
+});
