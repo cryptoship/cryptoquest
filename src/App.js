cae8cdc8795fb157678cf4c53e7af39022d78fa5
Derek Alia <derekalia@me.com>
2018-02-04 13:03:14 -0800

    added basic routes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import { withRouter } from 'react-router'
 
 import './App.css'
 
-import Index from './pages/Index'
+import Landing from './pages/Landing'
 import About from './pages/About'
+import Inventory from './pages/Inventory'
+import Play from './pages/Play'
+import Market from './pages/Market'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/about" component={About} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/about" component={About} />          
+          <Route exact path="/play" component={Play} />          
+          <Route exact path="/market" component={Market} />          
+          <Route exact path="/inventory" component={Inventory} />          
         </Switch>
       </div>
     )
---
+++ src/pages/Market/index.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react'
+export default class Index extends Component {
+    render() {
+      return (
+        <div>
+          <h2>Market</h2>
+          
+        </div>
+      )
+    }
+  }
+  
