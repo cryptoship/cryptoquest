9c4a1266fd26a4b693a608d99713c7ad41310a4c
Derek Alia <derekalia@me.com>
2018-02-04 19:42:09 -0800

    added character stuff

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 export default class Character extends Component {
+  state = {
+    name: 'Joe',
+    img: 'https://cdn.petcarerx.com/LPPE/images/articlethumbs/Cat-Scratch-Fever-Small.jpgs',
+    health: 7,
+    dex: 9,
+    wisdom: 8,
+    intel: 6,
+    strength: 12,
+    value: 23
+  };
   render() {
-    return <div>Character</div>;
+    return (
+      <div>
+        <div>
+          <div>Name:</div>
+          <div>{this.state.name}</div>
+          <div>health</div>
+          <div>{this.state.health}</div>
+          <div>dex</div>
+          <div>{this.state.dex}</div>
+          <div>intel</div>
+          <div>{this.state.intel}</div>
+          <div>wisdom</div>
+          <div>{this.state.wisdom}</div>
+          <div>ETH:</div>
+          <div>{this.state.value}</div>
+        </div>
+      </div>
+    );
   }
 }
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Character from './Character';
 
 export default class Characters extends Component {
   render() {
-    return <div>Characters</div>;
+    return (
+      <div>
+        Characters
+        <Character />
+      </div>
+    );
   }
 }
