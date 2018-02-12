ef31789e80e5c7c51fe589610c2f25e87e55c533
Daniel Kurka <kurka.daniel@gmail.com>
2018-02-11 16:52:45 -0800

    Improve build by outputting error messages.

--- solidity/compile.js
+++ solidity/compile.js
@@ -6,4 +6,23 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'CryptoQuest.sol');
 
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':CryptoQuest'];
+const solcOutputs = solc.compile(source, 1);
+
+if (solcOutputs && solcOutputs.errors) {
+    var errors = [];
+    for (var i = 0; i < solcOutputs.errors.length; i++) {
+        if (solcOutputs.errors[i].includes(': Warning:')) {
+            continue;
+        }        
+        errors.push(solcOutputs.errors[i]);
+    }
+    if (errors.length > 0) {
+        console.log("Compile failed");
+        for (var j = 0; j < errors.length; j++) {
+            console.log(errors[j]);
+        }
+        process.exit(-1);
+    }
+}
+
+module.exports = solcOutputs.contracts[':CryptoQuest'];
